8670218a46f0dbc402a3ecfa73d34c72f5a99bd3
Andres Hugueth <andreshugueth@gmail.com>
2020-02-06 20:23:35 +0000

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+c = 97;
+while (c <= 122)
+{
+putchar (c);
+c++;
+}
+putchar ('\n');
+return (0);
+}
