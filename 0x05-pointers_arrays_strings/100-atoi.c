d5c727b906e6fa653ae93d944cd0a16a29897ea5
Andres Hugueth <andreshugueth@gmail.com>
2020-02-19 04:01:48 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _atoi - convert a string to an integer.
+ * @s: Pointer
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	unsigned int n, i;
+	int x;
+
+	x = 1;
+	n = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] >= '9')
+		{
+			n = (s[i] - '0') + n * 10;
+			if (s[i+1] == ' ')
+			{
+				break;
+			}
+		}
+		else if (s[i] == '-')
+		{
+			x = x * -1;
+		}
+	}
+
+	return (n * x);
+}
