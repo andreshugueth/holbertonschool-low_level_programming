9fc7f5e8c7efb40c56eaa8dcaedffb4a98be29e7
Andres Hugueth <andreshugueth@gmail.com>
2020-02-20 15:29:17 +0000

    strcarb

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest: Destiny
+ * @src: source
+ * @n: bytes from src
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n; j++, ++i)
+	{
+		if (src[j] != '\0')
+		{
+			dest[i] = src[j];
+		}
+		else
+		{
+			break;
+		}
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
