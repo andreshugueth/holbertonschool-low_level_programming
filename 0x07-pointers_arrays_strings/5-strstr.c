816e0291a59536b17b13be8ecf20fed2737e7279
Andres Hugueth <andreshugueth@gmail.com>
2020-02-25 02:00:05 +0000

    '\0' fixed

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; needle[i] != '\0'; i++)
 		{
-			if (*haystack != needle[i])
+			if (*haystack != needle[i] || *haystack == '\0')
 			{
 				break;
 			}
