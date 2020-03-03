569916748263334f6cec176a218b599a277abe23
Andres Hugueth <andreshugueth@gmail.com>
2020-03-03 16:09:27 +0000

    string copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+ * _strdup - copies a given string
+ * @str: string
+ * Return: new string or NULL if its empty
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(char) * _strlen(str));
+	i = 0;
+	while (i < _strlen(str))
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = str[i];
+	return (s);
+}
+
