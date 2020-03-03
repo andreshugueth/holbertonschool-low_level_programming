cad1d7fafbeaa51a80338f6e761f24acf46827de
Andres Hugueth <andreshugueth@gmail.com>
2020-03-03 20:07:06 +0000

    2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * alloc_grid - 2 dimensional array of int
+ * @width: columns
+ * @height: rows
+ * Return: s or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **s;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(sizeof(int *) * height);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(sizeof(int) * width);
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+
+	return (s);
+}
+
