f9ecd6f4abcdf1873a7ae05b533486515589e32b
Andres Hugueth <andreshugueth@gmail.com>
2020-03-05 16:44:23 +0000

    string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: Pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size of second string to concat
+ * Return: concat string or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, l, m;
+	char *s3;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	l= _strlen(s1);
+	m= _strlen(s2);
+
+	if (n >= m)
+		n = m;
+
+	s3 = malloc(sizeof(char) * l + n + 1);
+	if (s3 == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++, k++)
+	{
+		s3[k] = s1[i];
+	}
+	for (j = 0; j < n; j++, k++)
+	{
+		s3[k] = s2[j];
+	}
+	s3[k] = '\0';
+	return (s3);
+}
+
