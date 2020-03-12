45188c948f30875d2aaef2cf1c6ae284e73afacf
Andres Hugueth <andreshugueth@gmail.com>
2020-03-12 02:39:32 +0000

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,12 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op != NULL)
+	while (i < 5)
 	{
 		if (*(ops[i].op) == *s)
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 	return (NULL);
