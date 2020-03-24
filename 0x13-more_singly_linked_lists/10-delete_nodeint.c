99ebc0a5e2087340b13a0c4506ea7badc9f1d371
Andres Hugueth <andreshugueth@gmail.com>
2020-03-24 23:59:15 +0000

    position corrected

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (*head != NULL)
 	{
-		if (i == (int) index)
+		if (i + 1 == (int) index)
 		{
 			temp = *head;
 			*head = (*head)->next;
