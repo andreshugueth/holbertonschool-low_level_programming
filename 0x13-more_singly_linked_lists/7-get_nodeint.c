1c1894df6fb50181dcbc1a1abe563a18f0f281fb
Andres Hugueth <andreshugueth@gmail.com>
2020-03-24 18:51:44 +0000

    fixed condition 2

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,17 +11,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	i = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	while (head != NULL)
 	{
-		head = head->next;
-		i++;
 		if (i == (int) index)
 			return (head);
+		head = head->next;
+		i++;
 	}
 	return (NULL);
 }
