3cdea47552052399158c46930117f14b08684d33
Andres Hugueth <andreshugueth@gmail.com>
2020-03-24 22:58:32 +0000

    header check deleted

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	int i;
 
 	i = 0;
-	if (*head == NULL)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
