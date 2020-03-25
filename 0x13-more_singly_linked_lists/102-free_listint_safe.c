4d388b6ec86760a33ed031d547d19b9b86f6e637
Andres Hugueth <andreshugueth@gmail.com>
2020-03-25 03:36:22 +0000

    free a list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a list
+ * @h: first element of the list
+ * Return: number of elements
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int i, sw;
+	listint_t *temp;
+
+	for (i = sw = 0; *h != NULL || sw; i++)
+	{
+
+		if (*h <=(*h)->next)
+		{
+			sw = 1;
+			break;
+		}
+
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
+		sw = 0;
+	}
+
+	if (sw)
+	{
+		temp = (*h)->next;
+		free(*h);
+		i++;
+	}
+	*h = NULL;
+	return (i);
+}
+
