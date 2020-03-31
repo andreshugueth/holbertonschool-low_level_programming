317bdee40d91c707cb96c5fd343222bc7debd5bb
Andres Hugueth <andreshugueth@gmail.com>
2020-03-31 21:26:01 +0000

    read file fixed bytes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	n_bytes = read(fd, buff, letters);
-	buff[letters] = '\0';
+	buff[n_bytes] = '\0';
 	close(fd);
 
 	if (n_bytes == 0)
