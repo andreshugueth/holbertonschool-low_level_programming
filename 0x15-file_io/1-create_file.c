82b3da4b73d8df4a99e6ac787425fc184d4c5813
Andres Hugueth <andreshugueth@gmail.com>
2020-04-01 01:29:57 +0000

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *create_file - creates a file
+ * @file_name: name of the file to be created
+ * @text_content: content of the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	ssize_t rt;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+
+	rt = write(STDOUT_FILENO, text_content, i);
+	close(fd);
+
+	if (rt == -1)
+		return (-1);
+
+	return (1);
+}
