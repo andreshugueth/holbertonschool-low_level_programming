522c51d0e62973e65117fc28b3bb3620421f9bd7
Andres Hugueth <andreshugueth@gmail.com>
2020-04-01 01:37:11 +0000

    open fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,12 +7,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i, rt;
+	int fd, i;
+	ssize_t rt;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
