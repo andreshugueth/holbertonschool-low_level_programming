87b7be9172f1995f55a8e1f90e4585ff7c4bc186
Andres Hugueth <andreshugueth@gmail.com>
2020-04-01 01:33:27 +0000

    fixed betty 1st

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- *create_file - creates a file
- * @file_name: name of the file to be created
+ * create_file - creates a file
+ * @filename: name of the file to be created
  * @text_content: content of the file
  * Return: 1 on success, -1 on failure
  */
@@ -32,3 +32,4 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
+
