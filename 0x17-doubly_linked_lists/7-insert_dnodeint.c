f877374d8ada04e1ef7e0067b2e5a5e5708f3fda
Andres Hugueth <andreshugueth@gmail.com>
2020-05-03 23:22:54 +0000

    fixed condition at end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,39 +9,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, *actual;
 	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-
-	i = 0;
-	new->n = n;
-
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	actual = *h;
+	for (i = 0; actual && i < idx; i++)
+		actual = actual->next;
+
+	if (!actual && i == idx)
+		return (add_dnodeint_end(h, n));
+	else if (actual)
 	{
-		new->next = *h;
-		new->prev = NULL;
-		(*h)->prev = new;
-		*h = new;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		actual->prev->next = new;
+		new->prev = actual->prev;
+		actual->prev = new;
+		new->next = actual;
 		return (new);
-	}
-	while (*h)
-	{
-		if (i + 1 == idx)
-		{
-			new->prev = *h;
-			(*h)->next->prev = new;
-			new->next = (*h)->next;
-			(*h)->next = new;
-			return (new);
-		}
-		h = &(*h)->next;
-		i++;
 	}
 	return (NULL);
 }
