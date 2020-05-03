1f44d6fa6e3516e0f01aae9a267af8099765909a
Andres Hugueth <andreshugueth@gmail.com>
2020-05-03 04:48:59 +0000

    nth node index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - gets the nth node of a lk list
+ * @head: copy of the head of the lk list
+ * @index: desired position of the node
+ *
+ * Return: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
