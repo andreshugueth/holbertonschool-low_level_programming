ac3071a5203a0d37a3495960beaf19968021bb5a
andreshugueth <andreshugueth@gmail.com>
2020-05-29 18:58:20 -0500

    fixing addition to the beginning

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,6 +28,41 @@ hash_node_t *ht_pair(const char *key, const char *value)
 	return (entry);
 }
 /**
+ * i_index - insert a node in a list
+ * @head: pointer to the list
+ * @key: given key
+ * @value: content of a key
+ *
+ * Return: the adress of a new node or NULL
+ */
+hash_node_t *i_index(hash_table_t **head, const char *key, const char *value)
+{
+	unsigned int index;
+	hash_node_t *new_node;
+
+	if (!head || !key || !value || strcmp(key, "") == 0)
+		return (NULL);
+	new_node = malloc(sizeof(hash_node_t));
+	index = key_index((unsigned char *)key, (*head)->size);
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (!(new_node)->key || !(new_node)->value)
+	{
+		if (new_node->key)
+			free(new_node->key);
+		if (new_node->value)
+			free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = (*head)->array[index];
+	(*head)->array[index] = new_node;
+	return (new_node);
+}
+
+/**
  * hash_table_set - sets a key & value to a ht
  * @ht: hash table
  * @key: id of the hash table
@@ -38,7 +73,7 @@ hash_node_t *ht_pair(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int slot;
-	hash_node_t *entry, *prev;
+	hash_node_t *entry;
 
 	if (!ht || !value || !key || strcmp(key, "") == 0)
 		return (0);
@@ -63,11 +98,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			entry->value = strdup(value);
 			return (1);
 		}
-		prev = entry;
-		entry = prev->next;
+		entry = entry->next;
 	}
 
-	prev->next = ht_pair(key, value);
+	if (!(i_index(&ht, key, value)))
+		return (0);
 	return (1);
 }
 
